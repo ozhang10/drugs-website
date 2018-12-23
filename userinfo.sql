2e5611fee40ebfbcbe4690ec288a5bd3c8d26e96
ozhang10 <42503379+ozhang10@users.noreply.github.com>
2018-12-23 16:19:38 -0500

    Update userinfo.sql

--- userinfo.sql
+++ userinfo.sql
@@ -4,9 +4,9 @@ TO DISK = 'D:\backups\userinfo.bak';
 
 CREATE TABLE user_info (
 	user_id INT AUTO_INCREMENT; 
-	username TINYTEXT PRIMARY KEY;
-	password VARCHAR(30) NOT NULL;
-	email VARCHAR PRIMARY KEY(255) CONSTRAINT LIKE '%@%(.com OR .org)'; /*need to constraint this to only allow alphanumeric string followed by @ and then .com*/
+	username VARCHAR(255) PRIMARY KEY;
+	password VARCHAR(255) NOT NULL;
+	email VARCHAR(255) PRIMARY KEY CONSTRAINT LIKE '%@%(.com OR .org)'; /*need to constraint this to only allow alphanumeric string followed by @ and then .com*/
 	first_name TINYTEXT NOT NULL;
 	last_name TINYTEXT NOT NULL;
 
